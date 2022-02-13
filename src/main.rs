adbfaf16956fdd7b558625c5baaf0eb0d360ac7b
Do Anh Tu <anhtu.do1998@gmail.com>
2022-02-13 17:22:19 +0800

    Added init function for CPU

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 struct YARCH8 {
-    pc: u16,
-    i: u16,
+    pc: u16, // only 12 bit = 4096 address possible
+    i: u16, // same
     ram: [u8; 4096],
     v_regs: [u8; 16],
     delay_timer: u8,
@@ -12,12 +12,29 @@ struct YARCH8 {
 
 impl YARCH8 {
     fn new() -> Self {
-        unimplemented!("Constructor not done!")
+        YARCH8 {
+            pc: 0x0,
+            i: 0x0,
+            ram: [0x0; 4096],
+            v_regs: [0x0; 16],
+            delay_timer: 0x0,
+            sound_timer: 0x0,
+            stack: [0x0; 16],
+            sp: 0x0,
+            disp_buff: [[false; 64]; 32],    
+        }
     }
 }
 
 fn main () {
     // Init CPU State (where pc, sp are ?)
-    // Read rom file into RAM
+    let cpu = YARCH8::new();
+    
+    // Read rom file into RAM (load program into memory)
+
+    // Loop
+
+    // Fetch
+    // Decode
     // Execute
 }
