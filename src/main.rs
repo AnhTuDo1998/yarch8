3b1d449799f86f5b2b9904451fb858fa272ff458
Do Anh Tu <anhtu.do1998@gmail.com>
2022-02-14 04:55:25 +0000

    Implement load rom to memory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
 struct YARCH8 {
     pc: u16, // only 12 bit = 4096 address possible
     i: u16, // same
@@ -24,13 +28,31 @@ impl YARCH8 {
             disp_buff: [[false; 64]; 32],    
         }
     }
+
+    fn load(&mut self, rom_path: &str) {
+        // Read from rom file and write into memory
+        let rom_file = File::open(rom_path).expect("Loading ROM error!");
+        
+        // Note that the lower 4 bits read in first
+        for (idx, byte) in rom_file.bytes().enumerate() {
+            // Memory start from 0x200 as original platform
+            self.ram[0x200 + idx] = byte.expect("Byte error in loading ROM!");
+        }
+    }
+
+    fn ram_peek(&self) {
+        println!("{:?}", self.ram);
+    }
 }
 
 fn main () {
     // Init CPU State (where pc, sp are ?)
-    let cpu = YARCH8::new();
+    let mut cpu = YARCH8::new();
     
     // Read rom file into RAM (load program into memory)
+    let rom_path = "ROM/ibm_logo.ch8";
+    cpu.load(rom_path);
+    cpu.ram_peek();
 
     // Loop
 
